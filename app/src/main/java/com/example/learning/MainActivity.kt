fbf349d7cc218dacff5c33873202c18bd6113161
neolink557 <neolink557@gmail.com>
2023-01-03 21:04:14 -0500

    test with columns, rows, image and texts

--- app/src/main/java/com/example/learning/MainActivity.kt
+++ app/src/main/java/com/example/learning/MainActivity.kt
@@ -3,41 +3,66 @@ package com.example.learning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.learning.ui.theme.LearningTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LearningTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyRows()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyRows() {
+    Row {
+        MyImage()
+        MyTexts()
+    }
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    LearningTheme {
-        Greeting("Android")
+fun MyTexts() {
+    Column {
+        MyText(text = "Hola Marge")
+        MyText(text = "Hola margot")
     }
+}
+
+@Composable
+fun MyImage() {
+    Image(
+        modifier = Modifier.size(150.dp),
+        painter = painterResource(R.drawable.bear),
+        contentDescription = "foreground"
+    )
+}
+
+@Composable
+fun MyText(text: String) {
+    Text(
+        modifier = Modifier.wrapContentWidth(Alignment.Start),
+        fontSize = 35.sp,
+        text = text)
+}
+
+@Preview
+@Composable
+fun PreviewTexts() {
+    MyRows()
 }
